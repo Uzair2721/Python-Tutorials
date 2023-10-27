9e698ae9503fa17160c32a530fd53e9731df15c4
Uzair2721 <134635584+Uzair2721@users.noreply.github.com>
2023-10-27 20:18:19 +0530

    Add files via upload

---
+++ venv.py
@@ -0,0 +1,7 @@
+# python -m venv myenv                         # to create virtual environment in folder
+# myenv\Scripts\activate.ps1                   # to activate the virtual environment
+# deactivate                                   # to deactivate the virtual environment
+# pip install                                  # to install any module or package
+# pip freeze                                   # to show all installed packages
+# pip freeze > requirements.txt                # to show all installed packages in text file
+# pip install -r requirements.txt              # to view and install all installed packages on a different device to whom the files are shared
